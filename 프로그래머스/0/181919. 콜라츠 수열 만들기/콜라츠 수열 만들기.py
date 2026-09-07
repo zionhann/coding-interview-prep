42a72e3eef90fe862ed908040e3a199134d1fec1
한시온 <its.zionhan@gmail.com>
2026-09-07 17:16:39 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = [n]
+    
+    while answer[-1] != 1:
+        if n % 2 == 0:
+            n //= 2
+
+        else:
+            n = 3 * n + 1
+        
+        answer.append(n)
+    
+    return answer
